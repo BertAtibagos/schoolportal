1750c1896685cde8160e3ff4c96fd26656f469dd
BertAtibagos <atibagos.robert2300@gmail.com>
2025-10-21 14:53:54 +0800

    Added modal for error message and record verification

--- dev/model/forms/tadi/prof/index.php
+++ dev/model/forms/tadi/prof/index.php
@@ -260,6 +260,41 @@
             </div>
         </div>
     </div>
+
+    <div class="modal fade" id="alertModal" tabindex="-1" aria-labelledby="alertModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="alertModalLabel">Notice</h5>
+            <!-- <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button> -->
+        </div>
+        <div class="modal-body" id="alertModalBody">
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        </div>
+        </div>
+    </div>
+    </div>
+
+    <!-- Add this to your HTML file -->
+    <div class="modal fade" id="confirmModal" tabindex="-1" aria-labelledby="confirmModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="confirmModalLabel">Confirm Verification</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            Are you sure you want to verify this record?
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+            <button type="button" class="btn btn-primary" id="confirmVerifyBtn">Verify</button>
+        </div>
+        </div>
+    </div>
+    </div>
 </section>
 <script src="tadi/prof/view/index-function.js?t=<?php echo time(); ?>"></script>
 <script src="tadi/prof/view/index-script.js?t=<?php echo time(); ?>"></script>
