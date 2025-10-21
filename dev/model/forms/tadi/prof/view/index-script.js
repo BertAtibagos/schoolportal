d7c9db0c907cafe056d6ec2e967ee2c44c2cdaed
BertAtibagos <atibagos.robert2300@gmail.com>
2025-10-21 14:56:01 +0800

    Added functions for modals and class list adder and remover

--- dev/model/forms/tadi/prof/view/index-script.js
+++ dev/model/forms/tadi/prof/view/index-script.js
@@ -8,9 +8,13 @@ document.getElementById("searchButton").addEventListener("click", function () {
     const searchQuery = document.getElementById("subjectSearch").value;
 
     if ((!lvlid || !yrlvlid || !prdid || !yrid) && !searchQuery) {
-        alert("Please select all the filters or enter a search query before searching.");
+        showAlertModal("Please select all the filters or enter a Subject Code before searching.");
+         emptyCriteriaReport();
         return;
+    }else{
+        resetCriteriaReport();
     }
+
     const formData = new FormData();
     formData.append('type', 'GET_SUBJECT_LIST');
     formData.append('lvl_id', lvlid);
@@ -51,8 +55,8 @@ document.querySelectorAll('.button-bg-change').forEach(btn => {
     btn.addEventListener("click", DISPLAYALL_TADI_RECORDS);
 });
 
-document.getElementById('date_srch').addEventListener("click", () => {
-    DISPLAY_TADI_LOG(document.getElementById('date_srch').value);
+document.getElementById('date_srch').addEventListener("click", function(){
+    DISPLAY_TADI_LOG(this.value);
 });
 
 
@@ -84,3 +88,95 @@ document.addEventListener("hidden.bs.modal", function (e) {
         backdrops.forEach(b => b.remove());
     }
 });
+
+function showAlertModal(message) {
+  const modalEl = document.getElementById('alertModal');
+  const modalBody = document.getElementById('alertModalBody');
+  modalBody.textContent = message;
+  const modal = new bootstrap.Modal(modalEl);
+  modal.show();
+}
+
+function emptyCriteriaReport(){
+    const academiclevel = document.getElementById('academiclevel');
+    const academicyearlevel = document.getElementById('academicYearLevel');
+    const academicperiod = document.getElementById('period');
+    const acadyear = document.getElementById('acadyear');
+    const subjCode = document.getElementById('subjectSearch');
+
+    academiclevel.classList.remove("border-dark");
+    academiclevel.classList.add("border-danger");
+    academiclevel.classList.add("is-invalid");
+
+    academicyearlevel.classList.remove("border-dark");
+    academicyearlevel.classList.add("border-danger");
+    academicyearlevel.classList.add("is-invalid");
+
+    academicperiod.classList.remove("border-dark");
+    academicperiod.classList.add("border-danger");
+    academicperiod.classList.add("is-invalid");
+
+    acadyear.classList.remove("border-dark");
+    acadyear.classList.add("border-danger");
+    acadyear.classList.add("is-invalid");
+
+    subjCode.classList.remove("border-dark");
+    subjCode.classList.add("border-danger");
+    subjCode.classList.add("is-invalid");
+}
+
+function resetCriteriaReport(){
+    const academiclevel = document.getElementById('academiclevel');
+    const academicyearlevel = document.getElementById('academicYearLevel');
+    const academicperiod = document.getElementById('period');
+    const acadyear = document.getElementById('acadyear');
+    const subjCode = document.getElementById('subjectSearch');
+
+    academiclevel.classList.remove("border-danger");
+    academiclevel.classList.remove("is-invalid");
+    academiclevel.classList.add("border-dark");
+    
+    academicyearlevel.classList.remove("border-danger");
+    academicyearlevel.classList.remove("is-invalid");
+    academicyearlevel.classList.add("border-dark");
+
+    academicperiod.classList.remove("border-danger");
+    academicperiod.classList.remove("is-invalid");
+    academicperiod.classList.add("border-dark");
+
+    acadyear.classList.remove("border-danger");
+    acadyear.classList.remove("is-invalid");
+    acadyear.classList.add("border-dark");
+
+    subjCode.classList.remove("border-danger");
+    subjCode.classList.remove("is-invalid");
+    subjCode.classList.add("border-dark");
+}
+
+function invalidStartDateInput(){
+    const startDate = document.getElementById("strtDateSearch");
+      startDate.classList.remove("border-dark");
+      startDate.classList.add("border-danger");
+      startDate.classList.add("is-invalid");
+}
+
+function invalidEndDateInput(){
+    const endDate = document.getElementById("endDateSearch");
+      endDate.classList.remove("border-dark");
+      endDate.classList.add("border-danger");
+      endDate.classList.add("is-invalid");
+}
+
+function resetStartEndDateInput(){
+    const startDate = document.getElementById("strtDateSearch");
+    const endDate = document.getElementById("endDateSearch");
+
+      startDate.classList.add("border-dark");
+      startDate.classList.remove("border-danger");
+      startDate.classList.remove("is-invalid");
+
+      endDate.classList.add("border-dark");
+      endDate.classList.remove("border-danger");
+      endDate.classList.remove("is-invalid");
+
+}
